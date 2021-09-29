e182f131c7682774701715d54a30626d9ffbbfdf
Anna Trubnikova <47085312+trubo4ka911@users.noreply.github.com>
2021-09-29 10:46:32 +0300

    Update

--- js/task.js
+++ js/task.js
@@ -1,172 +1,89 @@
 'use strict';
 
-import { default as imageRef } from "./gallery-items.js";
+const imageContainer = document.querySelector('.js-gallery');
+const imagesCards = createGallaryMarkup(galleryItems);
+const openModal = document.querySelector('.js-lightbox');
+const backdrop = document.querySelector('.lightbox__overlay');
+const imageRef = document.querySelector('.lightbox__image');
+const closeBtn = document.querySelector('[data-action="close-lightbox"]');
 
-class Gallery {
-	constructor(
-		imageList,
-		gallerySelector,
-		galleryItemSelector,
-		galleryLinkSelector,
-		galleryImageSelector,
-		lightBoxSelector,
-		lightBoxImageSelector,
-		closeButtonSelector,
-	) {
-		this.imageRef = imageList;
-		this.galleryEl = document.querySelector(gallerySelector);
-		this.liteBoxEl = document.querySelector(lightBoxSelector);
-		this.imageEl = document.querySelector(lightBoxImageSelector);
-		this.closeButtonEl = document.querySelector(closeButtonSelector);
-		this.galleryItemSelector = galleryItemSelector;
-		this.galleryLinkSelector = galleryLinkSelector;
-		this.galleryImageSelector = galleryImageSelector;
+imageContainer.insertAdjacentHTML('beforeend', imagesCards);
 
-		this.galleryEl.insertAdjacentHTML(
-			"afterBegin",
-			this.createImagesListString(),
-		);
-	}
-	createImagesListString() {
-		return this.imageRef.reduce(
-			(resultHTMLString, { preview, description, original }) => {
-				resultHTMLString += `<li class='${this.galleryItemSelector}'><a class='${this.galleryLinkSelector}' href='${original}'><img class='${this.galleryImageSelector}' src='${preview}' alt='${description}' data-source='${original}'></a></li>`;
+imageContainer.addEventListener('click', onImageContainerClick);
+closeBtn.addEventListener('click', onCloseClick);
+backdrop.addEventListener('click', onBackdropClick);
 
-				return resultHTMLString;
-			},
-			"",
-		);
-	}
-	setLightBoxImage({ original, description }) {
-		this.imageEl.src = original;
-		this.imageEl.alt = description;
-	}
-	clearLightBoxImage() {
-		this.imageEl.src = "";
-		this.imageEl.alt = "";
-	}
-	imageShift(imageObj, shift) {
-		this.imageEl.style.opacity = "0";
-		this.imageEl.style.transform = `translate3D(${shift}px, 0px, 0px)`;
-		setTimeout(() => {
-			this.clearLightBoxImage();
-			setTimeout(() => {
-				this.imageEl.style.transform = `translate3D(${-shift}px, 0px, 0px)`;
-			}, 1);
-		}, 150);
-		setTimeout(() => {
-			this.setLightBoxImage(imageObj);
-			this.imageEl.style.opacity = "1";
-			this.imageEl.style.transform = "translate3D(0px, 0px, 0px)";
-		}, 400);
-	}
-	nextImage() {
-		if (this.currentIndex === this.imageRef.length - 1) {
-			this.imageShift(this.imageRef[0], this.liteBoxEl.clientWidth);
-			this.currentIndex = 0;
-			return;
-		} else {
-			this.imageShift(
-				this.imageRef[this.currentIndex + 1],
-				this.liteBoxEl.clientWidth,
-			);
-			this.currentIndex += 1;
-			return;
-		}
-	}
-	previousImage() {
-		if (this.currentIndex === 0) {
-			this.imageShift(
-				this.imageRef[this.imageRef.length - 1],
-				-this.liteBoxEl.clientWidth,
-			);
-			this.currentIndex = this.imageRef.length - 1;
-			return;
-		} else {
-			this.imageShift(
-				this.imageRef[this.currentIndex - 1],
-				-this.liteBoxEl.clientWidth,
-			);
-			this.currentIndex -= 1;
-			return;
-		}
-	}
-	openLightBoxHandler(event) {
-		if (event.target.nodeName !== "IMG") {
-			return;
-		}
-		this.liteBoxEl.addEventListener("click", this.lightBoxHandler.bind(this));
-		document.addEventListener("keydown", this.keyboardHandler.bind(this));
-		event.preventDefault();
-		this.liteBoxEl.classList.add("is-open");
-		this.currentIndex = Array.from(this.galleryEl.children).indexOf(
-			event.target.closest("li"),
-		);
-		this.setLightBoxImage(
-			this.imageRef.find(item => item.original === event.target.dataset.source),
-		);
-	}
-	closeLightBox() {
-		this.liteBoxEl.removeEventListener(
-			"click",
-			this.lightBoxHandler.bind(this),
-		);
-		document.removeEventListener("keydown", this.keyboardHandler.bind(this));
-		this.liteBoxEl.classList.remove("is-open");
-		this.clearLightBoxImage();
-	}
-	lightBoxHandler(event) {
-		if (
-			event.target.dataset.action === "close-lightbox" ||
-			event.target.nodeName === "DIV"
-		) {
-			this.closeLightBox();
+function createGallaryMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+			return `
+		<li class="gallery__item">
+            <img class="gallery__image js_gallery_items" 
+            src = "${preview}"
+            data-source = "${original}"
+            alt = ${description}">
+        </li>
+		`;
+    })
+        .join('');
+};
 
-			return;
-		}
-		if (event.target.nodeName === "IMG") {
-			if (event.clientX > this.liteBoxEl.clientWidth / 2) {
-				this.nextImage();
-			} else {
-				this.previousImage();
-			}
-		}
-	}
-	keyboardHandler(event) {
-		if (this.liteBoxEl.classList.contains("is-open")) {
-			switch (event.key) {
-				case "Escape": {
-					this.closeLightBox();
-					return;
-				}
-				case "ArrowLeft": {
-					this.previousImage();
-					return;
-				}
-				case "ArrowRight": {
-					this.nextImage();
-					return;
-				}
-				default: {
-					return;
-				}
-			}
-		}
-	}
-}
+function onImageContainerClick(e) {
+    openModal.classList.add('is-open');
+    const imageUrl = e.target.getAttribute('data-source');
+    imageRef.src = imageUrl;
+    window.addEventListener('keyup', controlModuleWindow)
+};
 
-	const galleryObject = new Gallery(
-		imageRef,
-		".js-gallery",
-		"gallery__item",
-		"gallery__link",
-		"gallery__image",
-		".js-lightbox",
-		".js-lightbox .lightbox__image",
-		".js-lightbox button[data-action='close-lightbox']",
-	);
-	galleryObject.galleryEl.addEventListener(
-		"click",
-		galleryObject.openLightBoxHandler.bind(galleryObject),
-	);
+function closeModalWindow() {
+    openModal.classList.remove('is-open');
+    imageRef.src = "";
+    window.removeEventListener('keyup', controlModuleWindow);
+};
 
+function onCloseClick() {
+    closeModalWindow();
+};
+
+function onBackdropClick(e) {
+    if (e.target === e.currentTaget) {
+        closeModalWindow();
+    };
+};
+
+function controlModuleWindow(e) {
+    const keyNavigation = document.querySelectorAll('.js_gallery_items');
+    const imageArray = Array.from(keyNavigation);
+    const imageSrcArray = imageArray.map(image => {
+        const src = image.getAttribute('data-source');
+        return src;
+    });
+
+    let currentImageIndex = imageSrcArray.indexOf(imageRef.src);
+
+    const isEsc = e.keyCode === 27;
+    const isRight = e.keyCode === 39;
+    const isLeft = e.keyCode === 37;
+
+    switch (true) {
+        case isEsc:
+            closeModalWindow();
+            break;
+        
+        case isRight:
+            currentImageIndex += 1;
+            if (currentImageIndex > imageSrcArray.length-1) {
+                currentImageIndex = 0;
+			};
+            imageRef.src = imageSrcArray[currentImageIndex];
+            break;
+
+        case isLeft:
+            currentImageIndex -= 1;
+            if (currentImageIndex < 0) {
+                currentImageIndex = imageSrcArray.length - 1;
+			};
+            imageRef.src = imageSrcArray[currentImageIndex]
+            break;
+	};
+};
